f207056be39d74a44dde3a540d4d7112b07275e5
lakamson <lakamson@yahoo.com>
2021-01-03 16:59:10 -0800

    change image location

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/octoface.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-octocat.png`, // This path is relative to the root of the site.
       },
     },
   ],
